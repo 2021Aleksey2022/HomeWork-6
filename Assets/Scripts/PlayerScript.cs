36169ad0323a1a56ab1dda9351ae07e535db4325
Aleksey <walrus2022@mail.ru>
2022-12-05 22:26:00 +1000

    Добавил бомбу, откоретировал открывание двери и ничего не успеваю

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public bool key;
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag.Equals("Door"))
@@ -15,6 +14,10 @@ public class PlayerScript : MonoBehaviour
                 Vector3 rotation = other.gameObject.transform.eulerAngles;
                 rotation.y = -90;
                 other.gameObject.transform.eulerAngles = rotation;
+                Vector3 position = other.gameObject.transform.position;
+                position.x = -3.17f;
+                position.z = -2.95f;
+                other.gameObject.transform.position = position;
             }
         }
         if (other.tag.Equals("Key"))
@@ -26,7 +29,9 @@ public class PlayerScript : MonoBehaviour
         {
             Debug.Log("Boom!");
             other.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            other.gameObject.SetActive(false);
             gameObject.SetActive(false);
         }
     }
+ 
 }
